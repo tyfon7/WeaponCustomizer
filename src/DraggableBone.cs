e2627c9cdd1e914e305fa5fe47258bd5c1ba1c09
Tyfon <29051038+tyfon7@users.noreply.github.com>
2025-02-13 01:56:41 -0800

    multiple axes

--- src/DraggableBone.cs
+++ src/DraggableBone.cs
@@ -9,7 +9,10 @@ namespace WeaponCustomizer;
 
 public class DraggableBone : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    private const float MOVE_DISTANCE = 0.5f;
+    private const float LEFT_RIGHT_MOVE_DISTANCE = 0.5f;
+    private const float UP_DOWN_MOVE_DISTANCE = 0.2f;
+    private const float SIDE_MOVE_DISTANCE = 0.2f;
+
     private Image boneIcon;
     private Transform mod;
     private CustomizedMod customizedMod;
@@ -18,13 +21,9 @@ public class DraggableBone : MonoBehaviour, IDragHandler, IBeginDragHandler, IEn
     private CameraViewporter viewporter;
     private Action<bool> onChange;
 
-    private Vector3 originalLocalPosition;
-    private Vector3 minLocalPosition;
-    private Vector3 maxLocalPosition;
     private Vector2 minScreen;
     private Vector2 maxScreen;
-    private Transform rotator;
-    private Plane weaponPlane;
+    private Plane movementPlane;
 
     private bool dragging;
     private bool hovered;
@@ -37,25 +36,6 @@ public class DraggableBone : MonoBehaviour, IDragHandler, IBeginDragHandler, IEn
         this.slotId = slotId;
         this.viewporter = viewporter;
         this.onChange = onChange;
-
-        if (weapon.IsCustomized(slotId, out CustomPosition customPosition))
-        {
-            originalLocalPosition = customPosition.OriginalPosition;
-        }
-        else
-        {
-            originalLocalPosition = mod.localPosition;
-        }
-
-        // These bones get reinitialized when attachments are added, so we can't assume the gun is straight. The rotator has the rotation
-        rotator = mod.root.Find("Rotator");
-
-        // Currently everything is locked to left-right axis (forward/backward on the gun)
-        var direction = mod.parent.InverseTransformDirection(rotator.right);
-
-        // Arbitrary magnitude for the moment
-        minLocalPosition = originalLocalPosition - (MOVE_DISTANCE * direction);
-        maxLocalPosition = originalLocalPosition + (MOVE_DISTANCE * direction);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -121,16 +101,55 @@ public class DraggableBone : MonoBehaviour, IDragHandler, IBeginDragHandler, IEn
         if (customizedMod == null)
         {
             customizedMod = mod.gameObject.AddComponent<CustomizedMod>();
-            customizedMod.Init(originalLocalPosition, mod.localPosition);
+            customizedMod.Init(mod.localPosition, mod.localPosition);
         }
+
+        var originalLocalPosition = customizedMod.Customization.OriginalPosition;
+
+        bool shiftDown = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        bool ctrlDown = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+
+        var rotator = mod.root.Find("Rotator");
+
+        Vector3 upDirection = mod.parent.InverseTransformDirection(rotator.up);
+        Vector3 forwardDirection = mod.parent.InverseTransformDirection(rotator.forward);
+        Vector3 rightDirection = mod.parent.InverseTransformDirection(rotator.right);
+
+        float distance; // Max distance the mod is movable
+        Vector3 direction; // What direction it's moving in
+        Vector3 otherOffset; // How the mod has been moved on the *other* two axis
+
+        var offset = mod.localPosition - originalLocalPosition;
+        if (shiftDown)
+        {
+            direction = upDirection;
+            distance = UP_DOWN_MOVE_DISTANCE;
+            otherOffset = Vector3.Project(offset, forwardDirection) + Vector3.Project(offset, rightDirection);
+            movementPlane = new Plane(rotator.forward, mod.position);
+        }
+        else if (ctrlDown)
+        {
+            direction = forwardDirection;
+            distance = SIDE_MOVE_DISTANCE;
+            otherOffset = Vector3.Project(offset, upDirection) + Vector3.Project(offset, rightDirection);
+            movementPlane = new Plane(rotator.right, mod.position);
+        }
+        else
+        {
+            direction = rightDirection;
+            distance = LEFT_RIGHT_MOVE_DISTANCE;
+            otherOffset = Vector3.Project(offset, forwardDirection) + Vector3.Project(offset, upDirection);
+            movementPlane = new Plane(rotator.forward, mod.position);
+        }
+
+        var minLocalPosition = originalLocalPosition + otherOffset - (distance * direction);
+        var maxLocalPosition = originalLocalPosition + otherOffset + (distance * direction);
 
         Vector3 minPosition = mod.parent.TransformPoint(minLocalPosition);
         Vector3 maxPosition = mod.parent.TransformPoint(maxLocalPosition);
 
         minScreen = viewporter.TargetCamera.WorldToScreenPoint(minPosition);
         maxScreen = viewporter.TargetCamera.WorldToScreenPoint(maxPosition);
-
-        weaponPlane = new Plane(rotator.forward, mod.position);
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -152,7 +171,7 @@ public class DraggableBone : MonoBehaviour, IDragHandler, IBeginDragHandler, IEn
 
         // With that perfect screen position, raycast onto the weapon plane to find the exact spot where the mod should go
         Ray ray = viewporter.TargetCamera.ScreenPointToRay(screenPosition);
-        if (weaponPlane.Raycast(ray, out float enter))
+        if (movementPlane.Raycast(ray, out float enter))
         {
             Vector3 hitPoint = ray.GetPoint(enter);
             Vector3 localHitPoint = mod.parent.InverseTransformPoint(hitPoint);
